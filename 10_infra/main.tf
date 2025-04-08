1216de9af175cbfaf295469ec8fe2019e1c04884
sola72 <sola.color2@gmail.com>
2025-04-08 21:22:02 +0900

    S3バケットの追加

--- 10_infra/main.tf
+++ 10_infra/main.tf
@@ -10,10 +10,10 @@ terraform {
     }
   }
   backend "s3" {
-    bucket  = "{YOUR-S3-BUCKET-NAME}"
-    key     = "{YOUR-S3-BUCKET-KEY}"
+    bucket  = "mybucket-takanari-exe"
+    key     = "tastylog-dev.tfstate"
     region  = "ap-northeast-1"
-    profile = "terraform"
+    profile = "terraform01"
   }
 }
 
@@ -21,12 +21,12 @@ terraform {
 # Provider
 # ---------------------------------------------
 provider "aws" {
-  profile = "terraform"
+  profile = "terraform01"
   region  = "ap-northeast-1"
 }
 
 provider "aws" {
   alias   = "virginia"
-  profile = "terraform"
+  profile = "terraform01"
   region  = "us-east-1"
 }
